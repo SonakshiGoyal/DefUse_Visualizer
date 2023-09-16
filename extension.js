be33a47049ef7a8236a0b4a27c79bb83a3683e77
Spoorti Aparanji <88270835+spoorti016@users.noreply.github.com>
2023-09-16 16:57:16 +0530

    added highlighting and shortcut to call the extension

--- extension.js
+++ extension.js
@@ -11,7 +11,7 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-
+	
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
@@ -32,6 +32,10 @@ function activate(context) {
 			for (let i = 0 ;i <ddg.length; i++)
 			{
 				if (ddg[i][0]==highlighted && ddg[i][1]==currline+1){
+					const decoration = vscode.window.createTextEditorDecorationType({
+                        backgroundColor: 'yellow', // Your desired highlight color
+                        color: 'black' // Your desired text color
+                    });
 					console.log(String(ddg[i]))
 					let l = new Number(ddg[i][3]).valueOf() -1
 					
@@ -39,6 +43,11 @@ function activate(context) {
 					let ind = lines[l].indexOf(String(ddg[i][2]))
 					let pos1 = new vscode.Position(l,ind)
 					let pos2= new vscode.Position(l,ind+String(ddg[i][2]).length)
+					const rangeToHighlight = new vscode.Range(pos1, pos2);
+					editor.setDecorations(decoration, [rangeToHighlight]);
+
+                    // Optionally, you can remove the decoration after a delay or when needed.
+                    setTimeout(() => decoration.dispose(), 2000);
 
 					editor.selection =  new vscode.Selection(pos1,pos2);
 					editor.revealRange(range);
@@ -51,13 +60,20 @@ function activate(context) {
 		
 	});
 
+
+
 	let hopback = vscode.commands.registerCommand('hop.hopback', function () {
+		
 		const editor = vscode.window.activeTextEditor;
 		let lines = editor.document.getText().split("\n");
 		//const uri = vscode.Uri;
 		const selection = editor.selection;
 		const ddg = [["x", 17, "y", 18],["y", 18, "x", 6],["x", 6, "y", 7],["y", 23, "x", 6]]
 		if (selection && !selection.isEmpty) {
+			const decoration = vscode.window.createTextEditorDecorationType({
+				backgroundColor: 'yellow', // Your desired highlight color
+				color: 'black' // Your desired text color
+			});
 			const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
 			const highlighted = editor.document.getText(selectionRange).trim();
 			const currline = editor.selection.active.line;
@@ -72,6 +88,11 @@ function activate(context) {
 					let ind = lines[l].indexOf(String(ddg[i][0]))
 					let pos1 = new vscode.Position(l,ind)
 					let pos2= new vscode.Position(l,ind+String(ddg[i][0]).length)
+					const rangeToHighlight = new vscode.Range(pos1, pos2);
+					editor.setDecorations(decoration, [rangeToHighlight]);
+
+                    // Optionally, you can remove the decoration after a delay or when needed.
+                    setTimeout(() => decoration.dispose(), 2000);
 
 					editor.selection =  new vscode.Selection(pos1,pos2);
 					editor.revealRange(range);
@@ -86,6 +107,7 @@ function activate(context) {
 
 	context.subscriptions.push(hopforward);
 	context.subscriptions.push(hopback);
+	
 }
 
 // This method is called when your extension is deactivated
